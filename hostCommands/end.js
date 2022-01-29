f0638fc077cb7a73493a5262f31aad1ab6e69a29
anhhunglhp2904 <hoanganhhung29042004@gmail.com>
2022-01-29 17:25:01 +0700

    fix end

--- hostCommands/end.js
+++ hostCommands/end.js
@@ -6,9 +6,16 @@ module.exports={
     execute: async function(client, msg){
         await DB.updateObjectData('isGameStarted',[{isGameStarted:false}]);
         await DB.updateObjectData('day',[{index:1, dayNight:0}]);
+        await DB.update('players',[]);
+        await DB.update('playersID',[]);
+        await DB.update('prRole',[]);
         await DB.update('die', []);
+        await DB.update('mustDie', []);
         await DB.update('vote',[]);
         await DB.update('shield',[]);
+        await DB.updateObjectData('witchHealPotions', true);
+        await DB.updateObjectData('witchKillPotions', true);
+
         const embed = new MessageEmbed();
         embed.setTitle("-------------------------------End------------------------------- ");
         embed.setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`);
